86e61026c0ef7359fca5af46157a694315d3eebd
Guangda Ji <quanta.guangda.ji@gmail.com>
2023-09-13 22:38:11 +0200

    dd

--- core/pipeline/tsdf_feature.py
+++ core/pipeline/tsdf_feature.py
@@ -60,7 +60,7 @@ def tsdf_feature(args: ProgramArgs):
     tsdf_volume.reset_feature(
         dim=args.feat_dim,
         include_var=args.tsdf_feat_include_var,
-        dtype=getattr(torch, args.feature_dtype, None),
+        dtype=getattr(torch, args.feature_dtype) if args.feature_dtype is not None else None,
     )
 
     # start fusion
