cf5d4ab1216ab4385b78f97d534aff4e808dc1ab
Guangda Ji <quanta.guangda.ji@gmail.com>
2023-09-14 19:06:31 +0200

    switch to newer version of open3d for view control

--- env/install-conda.sh
+++ env/install-conda.sh
@@ -10,5 +10,7 @@ $HOME/.conda/envs/feat-seg-fusion/bin/pip install -r env/req-groungded-sam.txt
 $HOME/.conda/envs/feat-seg-fusion/bin/pip install -r env/req-mask-rcnn.txt
 
 # open3d for visualization
-$HOME/.conda/envs/feat-seg-fusion/bin/pip install open3d
+# $HOME/.conda/envs/feat-seg-fusion/bin/pip install open3d
+# this is the experimental version that have Visualizer.get_view_status() function implemented
+$HOME/.conda/envs/feat-seg-fusion/bin/pip install https://storage.googleapis.com/open3d-releases-master/python-wheels/open3d-0.17.0+5b6ef4b-cp310-cp310-manylinux_2_27_x86_64.whl 
 
--- env/install-slurm.sh
+++ env/install-slurm.sh
@@ -18,8 +18,8 @@ ${SCRATCH}/.python_venv/feat-seg-fusion/bin/pip3 install -r env/req-conceptfusio
 ${SCRATCH}/.python_venv/feat-seg-fusion/bin/pip3 install -r env/req-groungded-sam.txt --cache-dir ${SCRATCH}/pip_cache
 ${SCRATCH}/.python_venv/feat-seg-fusion/bin/pip3 install -r env/req-mask-rcnn.txt --cache-dir ${SCRATCH}/pip_cache
 
-# open3d
-${SCRATCH}/.python_venv/feat-seg-fusion/bin/pip3 install "https://github.com/quantaji/open3d-manylinux2014/releases/download/0.17.0/open3d_cpu-0.17.0-0minimal-cp310-cp310-manylinux_2_17_x86_64.whl"
+# open3d, headless rendering version
+${SCRATCH}/.python_venv/feat-seg-fusion/bin/pip3 install "https://github.com/quantaji/open3d-manylinux2014/releases/download/5b6ef4/open3d_cpu-0.17.0-0headless-cp310-cp310-manylinux_2_17_x86_64.whl"
 
 # deactivate
 deactivate
